7dd3f1b96433b37cb1beefc057b65487ce11e6a3
Ehizojie Ihayere <iszojie@gmail.com>
2023-03-13 11:00:32 +0100

    Initial Commit

---
+++ Finance365/Finance365App.swift
@@ -0,0 +1,17 @@
+//
+//  Finance365App.swift
+//  Finance365
+//
+//  Created by Ehizojie Ihayere on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Finance365App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
